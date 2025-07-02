8c53ccafc8412aa3b8b8792c3487aac0d4095b0a
oschijns <olivier.schyns95@gmail.com>
2025-07-02 19:42:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grid Collide
+
+Grid based collision engine for 2D and 3D retro gamedev.
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(unexpected_cfgs)]
+
+// cannot use 2D and 3D features at the same time
+#[cfg(all(feature = "2d", feature = "3d"))]
+compile_error!("The '2d' & '3d' features cannot be used at the same time.");
